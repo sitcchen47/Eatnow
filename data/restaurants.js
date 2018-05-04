449f6efd4d1feb3ba7b111070a2fa0652e2c73e6
Xin Song <xins2167@gmail.com>
2018-05-04 11:29:04 -0400

    add 2 restaurants in db

--- data/restaurants.js
+++ data/restaurants.js
@@ -1,28 +1,34 @@
 const restaurants = require("../dataDesign/restaurant");
-var router = require('express').Router();
 var multer = require('multer');
 var path = require('path');
 var DataModel = require('../routes/model');
-var fs = require('fs-extra');
-var path = require("path");
 var restaurantsModel = DataModel.Restaurants;
 var upload = multer({
     dest: path.join(__dirname, "../public/images")
 })
 
 for(let r of restaurants) {
-    console.log(r.name);
+    var addr = r.address.split(',');
+    if(addr.length === 4) {
+        addr.splice(1, 0, "");
+    }
     let rest = new restaurantsModel({
         name: r.name,
         tag: r.tag,
-        address: r.address.split(','),
+        address: {
+            address1: addr[0],
+            address2: addr[1],
+            city: addr[2],
+            state: addr[3],
+            zipcode: addr[4]
+        },
         contactInfo: {
             phoneNum: r.contactInfo.phoneNum,
             website: r.contactInfo.website
         },
         imgURL: r.imgURL,
         map : r.map,
-        createDate: new Date(),
+        createDate: r.createDate,
         editDate: new Date()
     });
     rest.save().then(() => {
